04d37e1bc79bffe78ae5af0b642459b7e75e5f8d
Arun <arunlokesh78@gmail.com>
2025-08-22 21:46:49 +0530

    changes-code

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,45 +1,5 @@
-// const Recipe = () => {
-//     return(
-//         <div id="recipe" class="recipe-list">
-//              <input type="search" placeholder="Type Your favorite Food"></input>
-//                 <button class="search" >Search</button>
-//                 <div class="display-section">
 
-//                 </div>
-//                 <div class="main-recipe">
-//                <div class="recipe-card">
-//                 <img src="Dosa.png"></img>
-//                 <p>Masala Dosa</p>
-//                 <button>Recipe</button>
-//                </div>
-//                <div class="recipe-card">
-//                 <img src="idli.png"></img>
-//                 <p>Idli</p>
-//                 <button>Recipe</button>
-//                </div>
-//                <div class="recipe-card">
-//                 <img src="pizza.jpg"></img>
-//                 <p>Margerita Pizza</p>
-//                 <button>Recipe</button>
-//                </div>
-//                <div class="recipe-card">
-//                 <img src="noddles.jpg"></img>
-//                 <p>Veg Noodles</p>
-//                 <button>Recipe</button>
-//                </div>
-//                <div class="recipe-card">
-//                 <img src="spageti.jpg"></img>
-//                 <p>Spagettei</p>
-//                 <button>Recipe</button>
-//                </div>
-//                </div>
-//         </div>
-//     )
-// }
-
-// export default Recipe;
-
-import React, { useState } from "react";
+import  { useState } from "react";
 import axios from "axios";
 
 const Recipe = () => {
@@ -67,6 +27,7 @@ const Recipe = () => {
           },
         }
       );
+
       setRecipes(res.data.results);
     } catch (err) {
       setError("Failed to fetch recipes. Please try again.");
